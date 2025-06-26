c54bef880ad84bfb6645abbd4c894c1f5c9641bb
fhhmm <takahashi.t66@gmail.com>
2025-06-26 22:38:56 +0900

    大まかな機能を仮実装

---
+++ app/controllers/DummyAuthController.scala
@@ -0,0 +1,91 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import play.api.libs.json.Json
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import java.util.Date
+
+@Singleton
+class DummyAuthController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  def authorize: Action[AnyContent] = Action { (request: Request[AnyContent]) =>
+    val query = request.queryString.map { case (k,v) => k -> v.mkString }
+    val clientId = query.getOrElse("client_id", "unknown")
+    val redirectUri = query.getOrElse("redirect_uri", "")
+    val state = query.getOrElse("state", "")
+    val code = "sample-code-1234"
+
+    val html =
+      s"""
+         |<html>
+         |<head><title>dummy authorization</title></head>
+         |<body>
+         |  <h1>【認可サーバー】権限付与(認可サーバーログイン)</h1>
+         |  <p>client_id: $clientId</p>
+         |  <p>redirect_uri: $redirectUri</p>
+         |  <p>state: $state</p>
+         |  <p>code: $code</p>
+         |  <form method="post" action="/callback">
+         |    <input type="hidden" name="redirect_uri" value="$redirectUri"/>
+         |    <input type="hidden" name="state" value="$state"/>
+         |    <input type="hidden" name="code" value="$code"/>
+         |    <button type="submit">クライアントに権限を付与</button>
+         |  </form>
+         |</body>
+         |</html>
+         |""".stripMargin
+
+    Ok(html).as(HTML)
+  }
+
+  def token: Action[AnyContent] = Action { (request: Request[AnyContent]) =>
+    val form = request.body.asFormUrlEncoded.getOrElse(Map.empty)
+
+    val grantType = form.get("grant_type").flatMap(_.headOption).getOrElse("")
+    val code = form.get("code").flatMap(_.headOption).getOrElse("")
+    val redirectUrl = form.get("redirect_url").flatMap(_.headOption).getOrElse("")
+    val codeVerifier = form.get("code_verifier").flatMap(_.headOption).getOrElse("")
+    val clientId = form.get("client_id").flatMap(_.headOption).getOrElse("client123")
+
+    // TODO: Validation
+
+    val secret = "my-secret-key-123456"
+    val algorithm = Algorithm.HMAC256(secret)
+    val now = System.currentTimeMillis()
+    val expiresInSec = 3600
+    val exp = new Date(now + expiresInSec * 1000)
+    val iat = new Date(now)
+    val nbf = new Date(now)
+    val iss = "http://localhost:9000"
+    val sub = "user-1234"
+    val name = "テストユーザー"
+    val email = "test@example.com"
+
+    val accessToken = JWT.create()
+      .withIssuer(iss)
+      .withSubject(sub)
+      .withAudience(clientId)
+      .withExpiresAt(exp)
+      .withIssuedAt(iat)
+      .sign(algorithm)
+
+    val idToken = JWT.create()
+      .withIssuer(iss)
+      .withSubject(sub)
+      .withAudience(clientId)
+      .withExpiresAt(exp)
+      .withIssuedAt(iat)
+      .withNotBefore(nbf)
+      .withClaim("name", name)
+      .withClaim("email", email)
+      .sign(algorithm)
+
+    Ok(Json.obj(
+      "access_token" -> accessToken,
+      "token_type" -> "Bearer",
+      "expires_in" -> expiresInSec.toString,
+      "id_token" -> idToken
+    ))
+  }
+}
--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -2,10 +2,127 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import play.api.libs.ws._
+import scala.concurrent.{ExecutionContext, Future}
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import com.auth0.jwt.JWTVerifier
+import play.api.libs.json.Json
 
 @Singleton
-class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def login = Action {
-    Redirect("/authorize?client_id=client123&redirect_uri=http://localhost:9000/callback&response_type=code&scope=openid&state=xyz&code_challenge=abc123&code_challenge_method=S256")
+class LoginController @Inject()(cc: ControllerComponents, ws: WSClient)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+  private val redirectUri = "/callback"
+  private val clientId = "client123"
+
+  def login: Action[AnyContent] = Action { (request: Request[AnyContent]) =>
+    val html =
+      s"""
+         |<html>
+         |<head><title>login</title></head>
+         |<body>
+         |  <h1>OIDCログイン</h1>
+         |  <form method="get" action="/dummyAuth/authorize">
+         |    <input type="hidden" name="client_id" value="client123"/>
+         |    <input type="hidden" name="redirect_uri" value="/callback"/>
+         |    <input type="hidden" name="response_type" value="code"/>
+         |    <input type="hidden" name="scope" value="openid"/>
+         |    <input type="hidden" name="state" value="xyz"/>
+         |    <input type="hidden" name="code_challenge" value="abc123"/>
+         |    <input type="hidden" name="code_challenge_method" value="S256"/>
+         |    <button type="submit">OIDCでログイン</button>
+         |  </form>
+         |</body>
+         |</html>
+         |""".stripMargin
+
+    Ok(html).as(HTML)
+  }
+
+  def callback: Action[AnyContent] = Action.async { (request: Request[AnyContent]) =>
+    val (code, state) = request.method match {
+      case "POST" =>
+        val form = request.body.asFormUrlEncoded.getOrElse(Map.empty)
+        val code = form.get("code").flatMap(_.headOption).getOrElse("")
+        val state = form.get("state").flatMap(_.headOption).getOrElse("")
+        (code, state)
+      case _ =>
+        val query = request.queryString.map { case (k, v) => k -> v.mkString }
+        val code = query.getOrElse("code", "")
+        val state = query.getOrElse("state", "")
+        (code, state)
+    }
+
+    val tokenRequest = ws.url("http://localhost:9000/dummyAuth/token")
+      .post(Map(
+        "grant_type" -> Seq("authorization_code"),
+        "code" -> Seq(code),
+        "redirect_uri" -> Seq(redirectUri),
+        "client_id" -> Seq(clientId),
+        "code_verifier" -> Seq("dummy")
+      ))
+
+    tokenRequest.map { wsResponse =>
+      val tokenResponseBody = wsResponse.body
+      val responseJson = Json.parse(tokenResponseBody)
+      val maybeIdToken = (responseJson \ "id_token").asOpt[String]
+
+      maybeIdToken match {
+        case Some(idToken) =>
+          Redirect("/mypage").withSession(
+            "id_token" -> idToken
+          )
+        case None =>
+          Redirect("/login")
+      }
+    }
+  }
+
+  def mypage: Action[AnyContent] = Action { request =>
+    val maybeIdToken = request.session.get("id_token")
+
+    maybeIdToken match {
+      case Some(idToken) =>
+        verifyAndDecodeIdToken(idToken) match {
+          case Right(jwt) =>
+            val sub = jwt.getSubject
+            val aud = jwt.getAudience
+            val exp = jwt.getExpiresAt
+            val name = jwt.getClaim("name").asString()
+            val email = jwt.getClaim("email").asString()
+
+            Ok(s"""
+                  |ログイン済みです。<br>
+                  |ユーザーID: $sub<br>
+                  |名前: $name<br>
+                  |メール: $email<br>
+                  |Audience: $aud<br>
+                  |有効期限: $exp
+          """.stripMargin).as("text/html; charset=UTF-8")
+
+          case Left(error) =>
+            Unauthorized(s"トークンの検証に失敗しました: $error")
+        }
+
+      case None =>
+        Redirect("/login")
+    }
+  }
+
+  private def verifyAndDecodeIdToken(idToken: String): Either[String, DecodedJWT] = {
+    try {
+      val algorithm = Algorithm.HMAC256("my-secret-key-123456")
+      val verifier: JWTVerifier = JWT
+        .require(algorithm)
+        .withIssuer("http://localhost:9000")
+        .withAudience("client123")
+        .build()
+
+      val decoded: DecodedJWT = verifier.verify(idToken)
+      Right(decoded)
+    } catch {
+      case e: Exception =>
+        Left(s"Invalid token: ${e.getMessage}")
+    }
   }
 }
